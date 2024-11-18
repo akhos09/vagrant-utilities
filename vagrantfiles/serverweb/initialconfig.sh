b015355e82920f5c29a777f43a64bf48abe2406c
akhos09 <150979654+akhos09@users.noreply.github.com>
2024-11-18 18:09:36 +0100

    Add files via upload

---
+++ vagrantfiles/serverweb/initialconfig.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Initial commands that are a must-execute.
+apt-get update && apt-get upgrade -y
---
+++ vagrantfiles/serverweb/services.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Commands to install al the required services.
+apt-get install apache2 -y  #INSTALLS APACHE
