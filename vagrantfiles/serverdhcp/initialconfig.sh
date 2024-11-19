047c475124109a9b76bc0407561da70f61f3b986
akhos09 <150979654+akhos09@users.noreply.github.com>
2024-11-19 02:09:36 +0100

    Update initialconfig.sh

--- vagrantfiles/serverdhcp/initialconfig.sh
+++ vagrantfiles/serverdhcp/initialconfig.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #Initial commands that are a must-execute.
 apt-get update && apt-get upgrade -y
+sudo echo "net.ipv4.ip_forward=1" > /etc/sysctl.conf
+sudo sysctl -p
 sudo iptables -t nat -A POSTROUTING -s 10.33.200.0/24 -o enp0s8 -j SNAT --to-source 172.19.200.67
