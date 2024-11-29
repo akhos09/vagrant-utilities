c4ee0f7ee40e95be3d1afe8bf0c1276e21f52b9a
Ak740s <pablofdezlpz09@gmail.com>
2024-11-29 20:26:18 +0100

    fix ansible path with pipx

--- vagrantfiles/ansible/pythonins.sh
+++ vagrantfiles/ansible/pythonins.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get install -y python3 python3-pip
+sudo apt-get install -y python3 python3-pip pipx
 python3 --version
+pipx ensurepath
 pip3 install ansible --user
 sudo ssh-keygen -t rsa -b 2048 -f keysshnodemaster
+sudo reboot
