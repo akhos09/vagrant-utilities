2525f73c72071c59693053236f1096f50fb02485
Ak740s <pablofdezlpz09@gmail.com>
2024-12-05 17:20:44 +0100

    key transfer for nodes and master node ssh added. With wget and an http.server

--- vagrantfiles/ansible/keygen.sh
+++ vagrantfiles/ansible/keygen.sh
@@ -1,4 +1,8 @@
-#!/bin/bash setup ssh
+#!/bin/bash
+#setup ssh
 sudo apt-get update && sudo apt-get upgrade -y
-cd /root/.ssh
-ssh-copy-id -i keysshnodemaster root@192.168.30.3
+echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
+echo -e "AuthorizedKeysFile .ssh/authorized_keys" >> /etc/ssh/sshd_config
+mkdir -p /root/.ssh && cd /root/.ssh
+wget http://192.168.30.3/id_rsa.pub
+cat id_rsa.pub >> authorized_keys
--- vagrantfiles/ansible/setup.sh
+++ vagrantfiles/ansible/setup.sh
@@ -4,6 +4,9 @@ sudo apt-get install -y python3 python3-pip pipx
 python3 --version
 pipx ensurepath
 pip3 install ansible --user
-sudo ssh-keygen -t rsa -b 2048 -f keysshnodemaster
-sudo reboot
+mkdir -p /root/.ssh && cd /root/.ssh
+echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
+echo -e "AuthorizedKeysFile .ssh/authorized_keys" >> /etc/ssh/sshd_config
+sudo ssh-keygen -t rsa -b 2048 -f id_rsa
+python3 -m http.server 80
 #all changes here
