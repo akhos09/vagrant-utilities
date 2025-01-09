9890a4ecf2a3c7a3b5d532e8cc248142d5dca6a7
Ak740s <pablofdezlpz09@gmail.com>
2025-01-09 16:39:18 +0100

    deleted as default the ansible user (un-comment the lines if you want to use it)

--- vagrantfiles/ansible/childsetup.sh
+++ vagrantfiles/ansible/childsetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SSH CONFIG ROOT MASTERNODE-------------------------------------------------------
+# SSH CONFIG ROOT MASTERNODE------------------------------------------------------------------------------------------------------------------------------------
 sudo apt-get update && sudo apt-get upgrade -y
 echo "PubkeyAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
 echo "AuthorizedKeysFile .ssh/authorized_keys" | sudo tee -a /etc/ssh/sshd_config
@@ -8,19 +8,19 @@ mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 cd /root/.ssh
 wget http://192.168.30.3/id_rsa.pub
-wget http://192.168.30.3/id_rsa2.pub
+# wget http://192.168.30.3/id_rsa2.pub
 cat id_rsa.pub >> authorized_keys
 chmod 600 authorized_keys
-#USER ANSIBLE CONFIG--------------------------------------------------------------
-useradd -m ansible -s /bin/bash
-echo "ansible:vagrant" | sudo chpasswd
-sudo chage -d 0 ansible
-mkdir -p /home/ansible/.ssh
-chmod 700 /home/ansible/.ssh
-mv /root/.ssh/id_rsa2.pub /home/ansible/.ssh
-cat /home/ansible/.ssh/id_rsa2.pub >> /home/ansible/.ssh/authorized_keys
-chmod 600 /home/ansible/.ssh/authorized_keys
-chown -R ansible:ansible /home/ansible/.ssh
-echo "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 sudo loadkeys es
-#ssh -i id_rsa2 ansible@192.168.30.4-6
+# USER ANSIBLE CONFIG (delete the # if you want an independent and additional ansible user)---------------------------------------------------------------------
+# useradd -m ansible -s /bin/bash
+# echo "ansible:vagrant" | sudo chpasswd
+# sudo chage -d 0 ansible
+# mkdir -p /home/ansible/.ssh
+# chmod 700 /home/ansible/.ssh
+# mv /root/.ssh/id_rsa2.pub /home/ansible/.ssh
+# cat /home/ansible/.ssh/id_rsa2.pub >> /home/ansible/.ssh/authorized_keys
+# chmod 600 /home/ansible/.ssh/authorized_keys
+# chown -R ansible:ansible /home/ansible/.ssh
+# echo "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+# ssh -i id_rsa2 ansible@192.168.30.4-6
--- vagrantfiles/ansible/setupmaster.sh
+++ vagrantfiles/ansible/setupmaster.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-#ANSIBLE INSTALL
+# ANSIBLE INSTALL-----------------------------------------------------------------------------------------------------------------------------------------------
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y python3 python3-pip pipx
 python3 --version
 pipx ensurepath
 pip3 install ansible --user
-#SSH ROOT
+# SSH ROOT------------------------------------------------------------------------------------------------------------------------------------------------------
 mkdir -p /root/.ssh && cd /root/.ssh
 echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
 echo -e "AuthorizedKeysFile .ssh/authorized_keys" >> /etc/ssh/sshd_config
 sudo ssh-keygen -t rsa -b 2048 -f id_rsa
-#SSH ANSIBLE USER
-useradd -m ansible -s /bin/bash
-echo "ansible:vagrant" | sudo chpasswd
-sudo chage -d 0 ansible
-sudo -u ansible mkdir -p /home/ansible/.ssh
-cd /home/ansible/.ssh
-sudo -u ansible ssh-keygen -f id_rsa2
-sudo mv ./id_rsa2.pub /root/.ssh/id_rsa2.pub
+# SSH ANSIBLE USER (delete the # if you want an independent and additional ansible user)------------------------------------------------------------------------
+# useradd -m ansible -s /bin/bash
+# echo "ansible:vagrant" | sudo chpasswd
+# sudo chage -d 0 ansible
+# sudo -u ansible mkdir -p /home/ansible/.ssh
+# cd /home/ansible/.ssh
+# sudo -u ansible ssh-keygen -f id_rsa2
+# sudo mv ./id_rsa2.pub /root/.ssh/id_rsa2.pub
 cd /root/.ssh/
-echo "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+# echo "ansible ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 python3 -m http.server 80 &
 mkdir -p /root/ansible/
 echo -e "192.168.30.4 node1\n192.168.30.5 node2\n192.168.30.6 node3" >> /etc/hosts
-echo -e "node1\nnode2\nnode3" > /root/ansible/machines
 echo -e "[clients]\nnode1\nnode2\nnode3" > /root/ansible/machines.ini
+echo -e "[defaults]\ninventory=./machines.ini" > /root/ansible/ansible.cfg
 sudo loadkeys es
-#ansible -i machines.txt -m shell -a "ls -la /root/ | grep 'file'" /with/bin/bash
-#ansible -i machines.txt -m command -a "ls -la /root/ | grep 'file'" /without/bin/bash
-#all changes here
+# ansible -i machines.txt -m shell -a "ls -la /root/ | grep 'file'" /with/bin/bash
+# ansible -i machines.txt -m command -a "ls -la /root/ | grep 'file'" /without/bin/bash
+# all changes here
