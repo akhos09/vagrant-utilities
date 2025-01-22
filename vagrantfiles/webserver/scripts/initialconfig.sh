5cd56301df554595b883d2788bcfaf44a0f98566
Ak740s <pablofdezlpz09@gmail.com>
2025-01-22 16:57:03 +0100

    Changed structure of serverweb template

--- vagrantfiles/webserver/scripts/initialconfig.sh
+++ vagrantfiles/webserver/scripts/initialconfig.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #Initial commands that are a must-execute.
 apt-get update && apt-get upgrade -y
+apt-get install apache2 -y
--- vagrantfiles/webserver/scripts/services.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Commands to install al the required services.
-apt-get install apache2 -y  #INSTALLS APACHE
