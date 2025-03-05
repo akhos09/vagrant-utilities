d6a0167e55f745c905b4335f00ffdc2845c126ca
Ak740s <pablofdezlpz09@gmail.com>
2025-03-05 20:02:13 +0100

    (WordPress) Added some minor changes and fixed some logic

--- vagrantfiles/wordpress/scripts/setup.sh
+++ vagrantfiles/wordpress/scripts/setup.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -x 
+mkdir /var/www/link
+sudo debconf-set-selections <<< 'grub-pc grub-pc/install_devices multiselect /dev/sda'
+sudo debconf-set-selections <<< 'grub-pc grub-pc/install_devices_disks_changed multiselect /dev/sda'
 
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install apache2 php mysql-server php-mbstring php-zip php-gd php-json php-curl openssl php-mysql -y
-#install phpmyadmin ln -s /usr/share/phpmyadmin
-sudo tee /etc/php/8.1/apache2/php.ini > /dev/null << 'EOF'
+sudo apt-get install apache2 php mysql-server php-mbstring php-zip php-gd php-json php-curl openssl libapache2-mod-php php-mysql -y
+#INTERACTIVE (DO IT MANUALLY)------------------------------------------------------------------------------------------------------
+# sudo apt-get install phpmyadmin -y
+# cd /var/www/html
+# sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
+
+sudo tee /etc/php/8.3/apache2/php.ini > /dev/null << 'EOF'
 display_errors = On
 display_startup_errors = On
 error_reporting = E_ALL
@@ -27,19 +34,19 @@ default_charset="ES"
 EOF
 
 sudo systemctl restart apache2
-echo "<?php phpinfo ();?>" > /var/www/html/version.php
 
 cd /etc/ssl/certs
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout server.key -out server.crt -subj "/CN=wppablo.net" -addext "subjectAltName=DNS:wppablo.net"
+
 sudo a2enmod ssl
-mkdir /var/www/wppablo.net
-cp /var/www/html/index.html /var/www/wppablo.net/index.html
+
 sudo tee /etc/apache2/sites-available/wppablo.net.conf > /dev/null << 'EOF'
 <VirtualHost *:443>
+    
     ServerAdmin webmaster@wppablo.net
     ServerName wppablo.net
     ServerAlias wppablo.net
-    DocumentRoot /var/www/wppablo.net
+    DocumentRoot /var/www/html
     
     SSLEngine on
 
@@ -55,11 +62,23 @@ sudo tee /etc/apache2/sites-available/wppablo.net.conf > /dev/null << 'EOF'
 
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
+
 </VirtualHost>
 EOF
+
+
 sudo a2ensite wppablo.net.conf
 sudo a2dissite 000-default.conf
 sudo systemctl restart apache2
+
 sudo cp /etc/ssl/certs/server.crt /usr/local/share/ca-certificates/server.crt
 sudo update-ca-certificates
-sudo reboot
+
+cd /var/www/html
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xvzf latest.tar.gz
+
+sudo chown -R www-data:www-data /var/www/html/wordpress
+sudo chmod -R 755 /var/www/html/wordpress
+sudo chown -R www-data:www-data /var/www/html
+sudo chown -R www-data:www-data /var/www/html/*
