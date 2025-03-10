8069156825c2a114c3f16c95dcc5ee34e2f6be74
Ak740s <pablofdezlpz09@gmail.com>
2025-03-10 17:55:13 +0100

    (WordPress) Added some minor changes (PHPMYADMIN and sync folder)

--- vagrantfiles/wordpress/scripts/setup.sh
+++ vagrantfiles/wordpress/scripts/setup.sh
@@ -6,12 +6,12 @@ sudo debconf-set-selections <<< 'grub-pc grub-pc/install_devices_disks_changed m
 
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install apache2 php mysql-server php-mbstring php-zip php-gd php-json php-curl openssl libapache2-mod-php php-mysql -y
-#INTERACTIVE (DO IT MANUALLY)------------------------------------------------------------------------------------------------------
+
+#INTERACTIVE INSTALL (DO IT MANUALLY cuz it wont work via vagrant)------------------------------------------------------------------------------------------------------
 # sudo apt-get install phpmyadmin -y
-# cd /var/www/html
 # sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
 
-sudo tee /etc/php/8.3/apache2/php.ini > /dev/null << 'EOF'
+sudo tee /etc/php/8.1/apache2/php.ini > /dev/null << 'EOF' #CONFIG PHP
 display_errors = On
 display_startup_errors = On
 error_reporting = E_ALL
@@ -19,8 +19,8 @@ log_errors = On
 error_log = /var/log/php_errors.log
 memory_limit = 256M
 max_execution_time = 300
-upload_max_filesize = 64M
-post_max_size = 64M
+upload_max_filesize = 4096M
+post_max_size = 4096M
 extension=phpmyadmin
 extension=curl
 extension=mbstring
@@ -35,12 +35,16 @@ EOF
 
 sudo systemctl restart apache2
 
-cd /etc/ssl/certs
+cd /etc/ssl/certs #HTTPS (IMPORT IT AFTER ALL INTO YOUR SYSTEM)
+#COMMAND IMPORT CERTIFICATE (PWSH ADMIN) 
+#Import-Certificate -FilePath "Path/server.crt" -CertStoreLocation Cert:\LocalMachine\Root
+
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout server.key -out server.crt -subj "/CN=wppablo.net" -addext "subjectAltName=DNS:wppablo.net"
+cp /etc/ssl/certs/server.crt /var/www/link/server.crt
 
 sudo a2enmod ssl
 
-sudo tee /etc/apache2/sites-available/wppablo.net.conf > /dev/null << 'EOF'
+sudo tee /etc/apache2/sites-available/wppablo.net.conf > /dev/null << 'EOF' #CONFIG VIRTUALHOST CHANGE WHATEVER YOU WANT
 <VirtualHost *:443>
     
     ServerAdmin webmaster@wppablo.net
@@ -82,3 +86,11 @@ sudo chown -R www-data:www-data /var/www/html/wordpress
 sudo chmod -R 755 /var/www/html/wordpress
 sudo chown -R www-data:www-data /var/www/html
 sudo chown -R www-data:www-data /var/www/html/*
+
+#CONFIG MYSQL WITH WORDPRESS PHPMYADMIN--------------------------------------------------------------------------------------------------------
+# sudo mysql -u root -p
+# CREATE DATABASE wordpress;
+# CREATE USER 'wordpressuser'@'localhost' IDENTIFIED BY 'password';
+# GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpressuser'@'localhost';
+# FLUSH PRIVILEGES;
+# EXIT;
